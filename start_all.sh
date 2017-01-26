c7432a9e2bae66954b0cf50a61fac1230791007a
Auguste Colle <auguste.colle@kuleuven.be>
2017-01-26 17:22:18 +0000

    bleeding working

--- start_all.sh
+++ start_all.sh
@@ -7,7 +7,7 @@ echo "Webserver initialized"
 
 echo "Initializing measurements"
 cd ./python/
-./app.py > /dev/null 2>&1 &
+python app.py > /dev/null 2>&1 &
 echo "Measurements initialized"
 
 ps aux | grep python
