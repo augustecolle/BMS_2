e8548a11cdb8bd5eac23fa2ae670b4d8c1456522
Auguste <augustecolle@hotmail.com>
2017-02-02 08:40:35 +0000

    .gitignore is now working

--- database/demo_plot.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-
-df = pd.read_csv('testnu.csv')
-df.plot(x='Timestamp', y=['Current', 'MVoltage', 'Sl1Voltage', 'Sl2Voltage', 'Sl3Voltage'])
-
-plt.show()
